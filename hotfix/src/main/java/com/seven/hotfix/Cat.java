2526ecc42ac0529aa6d4eb832a58712b29f0ec01
Seven2Qin <422107131@qq.com>
2018-11-28 09:36:06 +0800

    hotfix

---
+++ hotfix/src/main/java/com/seven/hotfix/Cat.java
@@ -0,0 +1,9 @@
+package com.seven.hotfix;
+
+public class Cat {
+
+    public String say() {
+        return "汪汪汪";
+    }
+
+}
--- hotfix/src/main/java/com/seven/hotfix/MainActivity.java
+++ hotfix/src/main/java/com/seven/hotfix/MainActivity.java
@@ -1,7 +1,13 @@
 package com.seven.hotfix;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn_Cat);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, new Cat().say(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        isGrantExternalRW();
+    }
+
+    public void isGrantExternalRW() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && MainActivity.this.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            MainActivity.this.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+        } else {
+
+        }
+
     }
 }
